f6dd3483c1264181cce859802c05503681dce0fd
Emp <mpt.s.a.melnikov@gmail.com>
2025-09-04 21:13:15 +0300

    feat: added support for macOS, iOSv13 and others

--- Package.swift
+++ Package.swift
@@ -5,7 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "SVGPathKit",
-    platforms: [.iOS(.v15)],
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
     products: [
         .library(
             name: "SVGPathKit",
